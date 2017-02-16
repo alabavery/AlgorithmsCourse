dd2bfae2b1d4b92fac73a7bb75e5849ace1f6239
Al Avery <averyab0@sewanee.edu>
2017-02-15 20:46:23 -0600

    Update GenericMST::find_next_union() and GenericMST::GenericMST()

--- src/MinimumSpanningTrees.cpp
+++ src/MinimumSpanningTrees.cpp
@@ -8,28 +8,62 @@
 #include "MinimumSpanningTrees.h"
 
 
-GenericMST::GenericMST(int N, string displaytreetxtpath) {
+GenericMST::GenericMST(int N, string displaytreetxtpath)
+{
 	this->N = N;
 	this->prime_node = 0;
 	this->uf = WeightedQuickUnion();
 	this->uf.init(this->N, displaytreetxtpath);
 
-	int counter = 0;
-	int p = 1;
-	while (p > 0) {
-		this->uf.union_and_print_wrapper(p, this->prime_node);
-		p = this->find_next_node();
-		counter++;
+	int the_union[2] = {0, 0};
+	while (true) {
+		this->find_next_union(the_union);
+
+		if (the_union[0] >= 0)
+		{
+			this->uf.union_and_print_wrapper(the_union[0], the_union[1]);
+		} else {
+			break;
+		}
 	}
 	this->uf.print_to_file();
 }
 
 
-int GenericMST::find_next_node() {
-	for (int i = 0; i < this->N; i++) {
-		if (this->uf.get_root(i) != this->prime_node) {
-			return i; // first we wont worry about minimum weighting
+void GenericMST::find_next_union(int the_union[2])
+{
+	int temp_union_0 = the_union[0];
+	int temp_union_1 = the_union[1];
+	int current_min_weight = 10000;
+
+	for (int node_outside = 1; node_outside < this->N; node_outside++)
+	{
+		if (this->uf.get_root(node_outside) != this->prime_node)
+		{
+			for (int node_inside = 0; node_inside < this->N; node_inside++)
+			{
+				if (this->uf.get_root(node_inside) == this->prime_node)
+				{
+					int this_weight = this->weighting_function(node_outside, node_inside);
+
+					if (this_weight < current_min_weight)
+					{
+						current_min_weight = this_weight;
+						the_union[0] = node_outside;
+						the_union[1] = node_inside;
+					}
+				}
+			}
 		}
 	}
-	return -1;
+
+	if (temp_union_0 == the_union[0] && temp_union_1 == the_union[1])
+	{
+		the_union[0] = -1;
+	}
+}
+
+
+int GenericMST::weighting_function(int p, int q) {
+	return p - q;
 }
--- src/MinimumSpanningTrees.h
+++ src/MinimumSpanningTrees.h
@@ -15,8 +15,9 @@ class GenericMST {
 private:
 	int N;
 	int prime_node;
-	int find_next_node();
+	void find_next_union(int the_union[2]);
 	WeightedQuickUnion uf;
+	int weighting_function(int p, int q);
 public:
 	GenericMST(int N, string displaytreetxtpath);
 };
